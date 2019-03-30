373e915970ab5eb5f07c29af0ce585de13d12045
0xfmc <45954870+0xfmc@users.noreply.github.com>
2019-03-30 22:02:43 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -1,10 +1,14 @@
 import discord
+import random
+import openpyxl
+import datetime
 import os
+import time
 
 
 
 client = discord.Client()
-
+times = int(time.time())
 
 
     
@@ -27,6 +31,29 @@ async def on_message(message):
      if message.author.bot:
           return None
         
+     if message.content.startswith("도움말"):
+        channel = message.channel
+        embed = discord.Embed(
+            title = '명령어들이다 크크크큭',
+            description = '베타입니다',
+            colour = discord.Colour.blue()
+        )
+
+        #embed.set_footer(text = '끗')
+        dtime = datetime.datetime.now()
+        #print(dtime[0:4]) # 년도
+        #print(dtime[5:7]) #월
+        #print(dtime[8:11])#일
+        #print(dtime[11:13])#시
+        #print(dtime[14:16])#분
+        #print(dtime[17:19])#초
+        embed.set_footer(text=str(dtime.year)+"년 "+str(dtime.month)+"월 "+str(dtime.day)+"일 "+str(dtime.hour)+"시 "+str(dtime.minute)+"분 "+str(dtime.second)+"초")
+        #embed.set_footer(text=dtime[0:4]+"년 "+dtime[5:7]+"월 "+dtime[8:11]+"일 "+dtime[11:13]+"시 "+dtime[14:16]+"분 "+dtime[17:19]+"초")
+        embed.add_field(name='도움말', value = '이메시지 생성!',inline = False)
+        embed.add_field(name='프로필 @멘션', value='프로필을 보여줌', inline=False)
+
+        await client.send_message(channel,embed=embed)  
+        
      if message.content.startswith('따라해'):
         if id in owner:
          learn = message.content.replace('따라해', "")
@@ -37,7 +64,6 @@ async def on_message(message):
         
      if message.content.startswith('say'):
         learn = message.content.replace('say', "")
-        await client.delete_message(message) 
         await client.send_message(message.channel,learn+'') 
         
      if message.content.startswith("프로필"):
@@ -57,7 +83,17 @@ async def on_message(message):
             embed.set_image(url=user.avatar_url)
             await client.send_message(message.channel, embed=embed)        
         
+     if message.content.startswith('공지'):
+        if id in owner:
+         learn = message.content.replace('공지', "")        
+         channel = discord.Object(id='527778051728343041')
+         await client.send_message(channel,learn+'')                       # await client.send_message(message.channel,learn+'')
+        else:
+          await client.send_message(channel,'권한이 없습니다!') 
         
+     if message.content.startswith('업타임'):
+          a = int(time.time())
+          await client.send_message(channel,str(a-times)+'초')         
 
 
 access_token = os.environ["BOT_TOKEN"] 
