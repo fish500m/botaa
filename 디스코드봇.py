cbf1734d889807f74f8c41f6627ce27c8d5fc66e
ㅁㄴㅇㄹ <45954870+0xfmc@users.noreply.github.com>
2019-04-02 21:53:22 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -1,10 +1,21 @@
-import discord 
 import setting
-import datetime
-import time
+import discord
 import asyncio
-import os
 import random
+import time
+from urllib.request import urlopen, Request
+import urllib
+import bs4
+from urllib.request import Request
+import openpyxl
+import re
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import os
+import sys
+import json
+from selenium import webdriver
 set = setting.set()
 
 
@@ -100,14 +111,127 @@ async def on_message(message):
          await app.send_message(message.channel,learn+'')     
         else:
           await app.send_message(channel,'')
+
+     if message.content.startswith('날씨'):
+        try:
+            embed = discord.Embed(title='잠시만요',description='불러오고있습니다!',color=0x00ff00)
+            meg = await app.send_message(channel,embed=embed)
+            learn = message.content.split(" ")
+            location = learn[1]
+            enc_location = urllib.parse.quote(location+'날씨')
+            hdr = {'User-Agent': 'Mozilla/5.0'}
+            url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+            req = Request(url, headers=hdr)
+            html = urllib.request.urlopen(req)
+            bs_obj = bs4.BeautifulSoup(html, "html.parser")
+            div = bs_obj.find("span",{"class":'todaytemp'})
+            div2 = bs_obj.find("p",{"class":"cast_txt"})
+            embed = discord.Embed(title=location+'날씨',description=div.text+'℃'+'\n'+div2.text+'\n'+'네이버 날씨',color=0x00ff00)
+            await app.edit_message(meg,embed=embed)
+        except:
+           await app.send_message(channel,'없는 도시입니다!')
+            
+     if message.content.startswith('미세먼지'):
+        try:
+           embed = discord.Embed(title='잠시만요',description='불러오고있습니다!',color=0x00ff00)
+           meg = await app.send_message(channel,embed=embed)
+           learn = message.content.split(" ")
+           location = learn[1]
+           enc_location = urllib.parse.quote(location+'미세먼지')
+           hdr = {'User-Agent': 'Mozilla/5.0'}
+           url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+           req = Request(url, headers=hdr)
+           html = urllib.request.urlopen(req)
+           bs_obj = bs4.BeautifulSoup(html, "html.parser")
+           div = bs_obj.find("em",{"class":'main_figure'})
+           div2 = bs_obj.find("span",{'class':'update'})
+           learn2 = int(div.text)
+           if learn2 < 16:
+               a = '최고'
+           elif learn2 < 31:
+               a = '좋음'
+           elif learn2 < 41:
+               a = '양호'
+           elif learn2 < 51:
+               a = '보통'
+           elif learn2 < 76:
+               a = '나쁨'
+           elif learn2 < 101:
+               a = '상당히 나쁨'
+           elif learn2 < 151:
+               a = '매우나쁨'
+           elif learn2 > 150:
+               a = '최악'
+           embed = discord.Embed(title=learn[1]+'미세먼지',description='**'+str(learn2)+'㎍/㎥ \n'+a+'**\n'+div2.text+'\n'+'네이버 미세먼지/미세먼지 기준 미세미세8단계',color=0x00ff00)
+           await app.edit_message(meg,embed=embed)
+        except:
+            await app.send_message(channel,'없는 도시 거나 도/시는 미세먼지 검색이 안됩니다.')
+
+     if message.content.startswith('청소'):
+        if id in owner:    
+             learn = message.content.split(' ')
+             mgs = []
+             number = int(learn[1])
+             async for x in client.logs_from(message.channel, limit = number+1):
+                  mgs.append(x)
+             await client.delete_messages(mgs)
+             delembed = discord.Embed(title=":speech_left: 메세지 삭제됨. (이 메세지는 3초 후 삭제됩니다)",color=0x4286f4)
+             delmsg = await client.send_message(channel,embed = delembed)
+             await asyncio.sleep(3)
+             await client.delete_message(delmsg)	
+        else:
+            await app.send_message(message.channel, "당신은 권환이 없습니다.")    	
 	
+     if message.content.startswith('초미세먼지'):
+            learn = message.content.split(' ')
+            embed = discord.Embed(title='잠시만요',description='불러오고있습니다!',color=0x00ff00)
+            meg = await app.send_message(channel,embed=embed)
+            location = learn[1]
+            enc_location = urllib.parse.quote(location+'초미세먼지')
+            hdr = {'User-Agent': 'Mozilla/5.0'}
+            url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+            req = Request(url, headers=hdr)
+            html = urllib.request.urlopen(req)
+            bs_obj = bs4.BeautifulSoup(html, "html.parser")
+            div = bs_obj.find("em",{"class":'main_figure'})
+            div2 = bs_obj.find("span",{'class':'update'})
+            learn2 = int(div.text)
+            if learn2 < 16:
+                a = '최고'
+            elif learn2 < 31:
+                a = '좋음'
+            elif learn2 < 41:
+                a = '양호'
+            elif learn2 < 51:
+                a = '보통'
+            elif learn2 < 76:
+                a = '나쁨'
+            elif learn2 < 101:
+                a = '상당히 나쁨'
+            elif learn2 < 151:
+                 a = '매우나쁨'
+            elif learn2 > 150:
+                 a = '최악'
+            embed = discord.Embed(title=learn[1]+'초미세먼지',description='**'+str(learn2)+'㎍/㎥ \n'+a+'**\n'+div2.text+'\n'+'네이버 초미세먼지/초미세먼지 기준 미세미세8단계',color=0x00ff00)
+            await app.edit_message(meg,embed=embed)	
 
   
 	
      if message.content.startswith('say'):
         learn = message.content.replace('say', "")
         await app.send_message(message.channel,learn+'') 
-        
+
+	
+     if message.content.startswith('정보'):
+           date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+           embed = discord.Embed(color=0x00ff00)
+           embed.add_field(name="이름",value=message.author.name,inline=True)
+           embed.add_field(name='서버닉네임',value=message.author.display_name, inline=True)
+           embed.add_field(name='가입일',value=str(date.year) +'년' +str(date.month) + '월' + str(date.day) + '일', inline=True)
+           embed.add_field(name='아이디',value=message.author.id,inline=True)
+           embed.set_thumbnail(url=message.author.avatar_url)
+           await app.send_message(channel,embed=embed)      
+	
      if message.content.startswith("프로필"):
 
         text = ""
@@ -214,7 +338,7 @@ async def on_message(message):
             embed.set_footer(text="봇 공지")
             await app.edit_message(mssg, embed=embed)
         else:
-            await app.send_message(message.channel, "봇 제작자만 사용할수 있는 커맨드입니다!")            
+            await app.send_message(message.channel, "당신은 권환이 ")            
 
             
       
