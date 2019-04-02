eb94f84cbe1ea02fcd388c0f124c3af5c84d8cd4
ㅁㄴㅇㄹ <45954870+0xfmc@users.noreply.github.com>
2019-04-02 20:33:30 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -26,11 +26,103 @@ async def on_message(message):
 
      id = message.author.id
      channel = message.channel
-     owner = ['480568196286644224','375951816384446464','536133196706873352','558893178732806147']
+     owner = ['480568196286644224','375951816384446464','536133196706873352']
      if message.author.bot:
           return None
     
      if message.author.id == app.user.id: return
+
+
+     print("Channel: %s(%s) | Author: %s(#%s) | Message: %s" % (
+            message.channel, str(message.channel.id)[:5],
+            message.author.name, str(message.author.id),
+            message.content
+	    	))
+	
+     if message.content.startswith('봇게임'):
+        if id in owner:         
+           learn = message.content.replace('봇게임', "")
+           await app.change_presence(game=discord.Game(name=learn))
+           await app.send_message(message.channel, "봇의 플래이중을 바꿨습니다.")
+        else:
+           await app.send_message(channel,'')
+	
+     if message.content.startswith('서버'):
+
+         list = []
+         for server in app.servers:
+             list.append(server.name)
+         await app.send_message(message.channel, "\n".join(list))
+
+     if message.content.startswith('시간'):
+        channel = message.channel
+        embed = discord.Embed(colour = discord.Colour.blue())
+
+        #embed.set_footer(text = '끗')
+        dtime = datetime.datetime.now()         
+        embed.set_footer(text=str(dtime.year)+"년 "+str(dtime.month)+"월 "+str(dtime.day)+"일 "+str(dtime.hour)+"시 "+str(dtime.minute)+"분 "+str(dtime.second)+"초")
+        await app.send_message(channel,embed=embed)
+
+
+
+     if message.content.startswith('업타임'):
+          a = int(time.time())
+          await app.send_message(channel,str(a-times)+'초')
+          
+     if message.content.startswith("도움말"):
+        channel = message.channel
+        embed = discord.Embed(
+            title = '명령어들이다 크크크큭',
+            description = '베타입니다',
+            colour = discord.Colour.blue()
+        )
+
+        #embed.set_footer(text = '끗')
+        dtime = datetime.datetime.now()
+        #print(dtime[0:4]) # 년도
+        #print(dtime[5:7]) #월
+        #print(dtime[8:11])#일
+        #print(dtime[11:13])#시
+        #print(dtime[14:16])#분
+        #print(dtime[17:19])#초
+        embed.set_footer(text=str(dtime.year)+"년 "+str(dtime.month)+"월 "+str(dtime.day)+"일 "+str(dtime.hour)+"시 "+str(dtime.minute)+"분 "+str(dtime.second)+"초")
+        #embed.set_footer(text=dtime[0:4]+"년 "+dtime[5:7]+"월 "+dtime[8:11]+"일 "+dtime[11:13]+"시 "+dtime[14:16]+"분 "+dtime[17:19]+"초")
+        embed.add_field(name='도움말', value = '이메시지 생성!',inline = False)
+        embed.add_field(name='프로필 @멘션', value='프로필을 보여줌', inline=False)
+
+        await app.send_message(channel,embed=embed)  
+        
+     if message.content.startswith('따라해'):
+        if id in owner:
+         await app.delete_message(message)            
+         learn = message.content.replace('따라해', "")
+         await app.send_message(message.channel,learn+'')     
+        else:
+          await app.send_message(channel,'')
+	
+
+  
+	
+     if message.content.startswith('say'):
+        learn = message.content.replace('say', "")
+        await app.send_message(message.channel,learn+'') 
+        
+     if message.content.startswith("프로필"):
+
+        text = ""
+
+        learn = message.content.split(" ")
+
+        vrsize = len(learn)
+
+        vrsize = int(vrsize)
+
+        for i in range(1, vrsize):
+            text = text + " " + learn[i]
+        for user in message.mentions:
+            embed = discord.Embed(color=0xffffff)
+            embed.set_image(url=user.avatar_url)
+            await app.send_message(message.channel, embed=embed)  
 
 
 
