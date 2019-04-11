3b58e41962b2ae0be10be7e86f38f5eb32a4b2b6
ㅁㄴㅇㄹ <45954870+0xfmc@users.noreply.github.com>
2019-04-11 20:06:19 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,6 +1,10 @@
+"""
+Github GNU General Public License version 3.0 (GPLv3)
+Copyright 매리 2018, All Rights Reserved
+"""
 
-import datetime
-
-class Settings:
+class set:
     def __init__(self):
-        self.copy = "© %s Team Melon" % datetime.datetime.now().year
+
+        self.nfct = False # ( True : 채널 생성후 발송, False : 아무것도 안함 )
+
