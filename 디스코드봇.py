98ef5e27910f622cafcfea9e96536db976803949
ㅁㄴㅇㄹ <45954870+0xfmc@users.noreply.github.com>
2019-04-18 19:23:29 +0900

    Update 디스코드봇.py

--- 디스코드봇.py
+++ 디스코드봇.py
@@ -97,8 +97,8 @@ async def on_message(message):
          await app.send_message(message.channel,learn+'')
 	
      if message.content.startswith('_홍보'):
-         embed.add_field(name = '서버주소', value = '[[개발자 서버](https://invite.gg/logserver)]',inline = False)
-         await app.send_message(message.channel, embed=embed)
+         embed = discord.Embed(title='서버주소', description='[[개발자 서버](https://invite.gg/logserver)]', color=0x00ff00)
+         await app.send_message(message.channel, embed=embed))
     
      if message.author.id == app.user.id: return
 	
